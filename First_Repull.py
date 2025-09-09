d90607fb315c96582dd22d89ed3652d44bf50abe
Pinaki Ghosh <pinaki.ghosh@duke.edu>
2025-09-09 14:38:04 -0400

    Updated changes as per review comments

--- First_Repull.py
+++ First_Repull.py
@@ -4,8 +4,8 @@ var_data = pd.read_csv("https://github.com/nickeubank/MIDS_Data/raw/refs/heads/m
 var_data.head()
 var_data.columns
 columns = var_data[['Mortality rate, infant (per 1,000 live births)','GDP per capita (constant 2010 US$)', 'Country Name']]
-plt.figure(figsize = (10,6))
-plt.scatter(columns['GDP per capita (constant 2010 US$)'], columns['Mortality rate, infant (per 1,000 live births)'], alpha= 0.7)
+plt.figure(figsize = (15,9))
+plt.scatter(columns['GDP per capita (constant 2010 US$)'], columns['Mortality rate, infant (per 1,000 live births)'], alpha= 0.4)
 plt.xlabel('GDP per capita')
 plt.ylabel('Mortality rate, infant')
 plt.title('GDP vs Mortality rate')
